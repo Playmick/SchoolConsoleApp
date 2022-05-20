55d95b87b072f59a702c381e789311503fbf1ea6
tk <tk@npcat.ru>
2022-05-20 18:00:51 +0500

    Try to make event

--- BaseTeacher.cs
+++ BaseTeacher.cs
@@ -8,7 +8,18 @@ namespace SchoolProgram
 {
     abstract class BaseTeacher : ITeacher
     {
-        string Name;
+        private string name;
+        private Action EndOfLesson;
+        
+        //конструктор
+        public BaseTeacher(string nameOfTeacher)
+        {
+            name = nameOfTeacher;
+
+            EndOfLesson = MakeReviewWorkToAllStudents;
+            EndOfLesson += TakeHomeWorkForStudents;
+            EndOfLesson += TakeFreedomToAllStudents;
+        }
 
         public void MakeReviewWorkToAllStudents()
         {
